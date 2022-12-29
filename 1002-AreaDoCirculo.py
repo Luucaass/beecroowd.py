5ad5dbc5f45ed8f5704996da8ec82bd5ebd1ffa7
Lucas Gabriel <99739250+Luucaass@users.noreply.github.com>
2022-12-29 19:34:48 -0300

    Create 1002-AreaDoCirculo.py

---
+++ 1002-AreaDoCirculo.py
@@ -0,0 +1,11 @@
+n = 3.14159
+raio = float(input())
+
+num = n * raio**2
+
+area = round(num, 4)
+
+print('A={area}'.format(area=area))
+
+#este codigo está imprimindo exato oque o problema solicita, dando resposta errada,
+#se conseguir copie o codigo e edite ele pra conseguir compilar no bee.
